ba3b1b1188d0cd4e0a764fc630597f2eb868b55f
[LG유플러스] 윤성민 <joypop@lguplus.co.kr>
2024-02-03 18:05:17 +0900

    5강. 제네릭 제약과 제네릭 함수 메인메소드 구현

--- src/main/kotlin/generic/Cage5.kt
+++ src/main/kotlin/generic/Cage5.kt
@@ -1,50 +1,49 @@
 package generic
 
 fun main() {
-
+    val cage = Cage5(mutableListOf(Sparrow(), Eagle()))
+    cage.printAfterSorting()
 }
 
 abstract class Bird(
-  name: String,
-  private val size: Int,
+    name: String,
+    private val size: Int,
 ) : Animal(name), Comparable<Bird> {
-  override fun compareTo(other: Bird): Int {
-    return this.size.compareTo(other.size)
-  }
+    override fun compareTo(other: Bird): Int {
+        return this.size.compareTo(other.size)
+    }
 }
 
 class Sparrow : Bird("참새", 100)
+
 class Eagle : Bird("독수리", 500)
 
-
 class Cage5<T>(
-  private val animals: MutableList<T> = mutableListOf(),
+    private val animals: MutableList<T> = mutableListOf(),
 ) where T : Animal, T : Comparable<T> {
+    fun printAfterSorting() {
+        this.animals.sorted()
+            .map { it.name }
+            .let { println(it) }
+    }
 
-  fun printAfterSorting() {
-    this.animals.sorted()
-      .map { it.name }
-      .let { println(it) }
-  }
+    fun getFirst(): T {
+        return animals.first()
+    }
 
-  fun getFirst(): T {
-    return animals.first()
-  }
+    fun put(animal: T) {
+        this.animals.add(animal)
+    }
 
-  fun put(animal: T) {
-    this.animals.add(animal)
-  }
+    fun moveFrom(otherCage: Cage5<T>) {
+        this.animals.addAll(otherCage.animals)
+    }
 
-  fun moveFrom(otherCage: Cage5<T>) {
-    this.animals.addAll(otherCage.animals)
-  }
-
-  fun moveTo(otherCage: Cage5<T>) {
-    otherCage.animals.addAll(this.animals)
-  }
+    fun moveTo(otherCage: Cage5<T>) {
+        otherCage.animals.addAll(this.animals)
+    }
 }
 
 fun <T> List<T>.hasIntersection(other: List<T>): Boolean {
-  return (this.toSet() intersect other.toSet()).isNotEmpty()
+    return (this.toSet() intersect other.toSet()).isNotEmpty()
 }
-
